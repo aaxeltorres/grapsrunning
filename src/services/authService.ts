0a291825062b228618ac566b800caf631b18809d
aaxeltorres <aaxeltorres1@gmail.com>
2025-08-12 17:54:13 -0300

    Route change /api/auth/login to /api/users/login

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -31,7 +31,7 @@ export const loginUser = async (formData: {
   email: string;
   password: string;
 }) => {
-  const response = await fetch(`${API_URL}/api/auth/login`, {
+  const response = await fetch(`${API_URL}/api/users/login`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(formData),
