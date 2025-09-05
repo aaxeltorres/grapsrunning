9efa3674b5c166560d6804d8fdfad64f62f13efc
aaxeltorres <aaxeltorres1@gmail.com>
2025-09-05 14:49:57 -0300

    Routes Update

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? './' : '/',
+export default defineConfig({
+  base: "/", // siempre desde raíz para Vercel
   plugins: [react()],
-}));
+});
